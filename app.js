7550600e255b235e34408b3c56ee35151c41a500
sadaf0611 <122720925+sadaf0611@users.noreply.github.com>
2023-01-16 20:12:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,3 +19,22 @@ var headerTitle=document.getElementById('header-title');
 console.log(headerTitle);
 headerTitle.textContent='Hello';
 headerTitle.innerText='Good Bye';
+
+// console.log(headerTitle.textContent);
+//console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>header 3 </h3>';
+// headerTitle.style.borderBottom='solid 3px black';
+// header.style.borderBottom='solid 3px black';
+
+var items=document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[1]);
+items[1].textContent='hello';
+items[1].style.fontWeight='bold'; 
+items[1].style.backgroundColor='yellow';
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor='pink'; 
+}
+items[2].style.backgroundColor='pink'; 
