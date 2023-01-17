63ebffd88d9f2fb2b6c983e49f600ee5220e99f6
sadaf0611 <122720925+sadaf0611@users.noreply.github.com>
2023-01-17 12:40:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,3 +51,15 @@ for(var i=0;i<li.length;i++)
 {
     li[i].style.fontWeight='bold'; 
 } 
+// var items=document.getElementsByTagName("li");
+// items[1].style.backgroundColor='green';
+// items[2].style.color='white';
+
+var item = document.querySelector('.list-group-item:nth-child(2)');
+item.style.color='green';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='green'; 
+}
